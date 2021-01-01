4433bb1b8db2f17f89cf80095003d3ae0ed262ee
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-01-01 17:59:11 +0900

    initial commit
    練習

---
+++ lesson06.rb
@@ -0,0 +1,12 @@
+num = 1
+num01 = 2
+# num-01 = 3
+
+p num
+print num01
+# p num-002
+
+
+# return = 10
+
+# p return
---
+++ lesson07.rb
@@ -0,0 +1,23 @@
+# p 10
+
+# p 10.div(5)
+# p 10.remainder(3)
+
+# p 10.class
+# p 10.0.class
+
+# puts "Good Mornig".index("d")
+# puts "Good Mornig".length
+# puts "Good Morning".class
+
+# print 10>1
+# puts (10>1).class
+# p (10>1).class
+
+# p 10<1
+# p (10<1).class
+
+num1 = 1
+num2 = 2
+
+p num1>num2
